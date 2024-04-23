ce0086f7acada87c1ba4304301d6114c222bfbad
Leah Deshar <leah.desar@gmail.com>
2024-04-24 01:08:58 +0545

    step 4 completed

---
+++ djangowebsocket/chat/ChatApp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Room(models.Model):
+    room_name = models.CharField(max_length=255)
+
+    def __str(self):
+        return self.room_name
+    
+    
+class Message(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    sender = models.CharField(max_length=255)
+    message = models.TextField()
+
+    def __str(self):
+        return str(self.room)
--- djangowebsocket/chat/chat/settings.py
+++ djangowebsocket/chat/chat/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ChatApp',
+    'channels',
+    
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,9 @@ ROOT_URLCONF = 'chat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'template'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

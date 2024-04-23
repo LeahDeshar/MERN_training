64664d3ff9547539a8e0f022c8aa58cf3eed2753
Leah Deshar <leah.desar@gmail.com>
2024-04-23 13:02:06 +0545

    multiplayer

--- websocket/server/server.js
+++ websocket/server/server.js
@@ -4,17 +4,14 @@ const { Server } = require("socket.io");
 const httpServer = createServer();
 const socket = new Server(httpServer, {
   cors: {
-    origin: "http://127.0.0.1:5500",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
 
 socket.on("connection", (socket) => {
   //   console.log(socket);
-  socket.on("MessageReq", (data) => {
-    console.log("MessageReq in server", data);
-  });
-  socket.emit("message", "Hello from server");
+  console.log("connected");
 });
 
 httpServer.listen(3000, () => {

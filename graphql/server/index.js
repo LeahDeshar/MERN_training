ba5572a5c766b7a918cca06930b07e992555590e
Leah Deshar <leah.desar@gmail.com>
2024-04-25 23:07:48 +0545

    graphql training started

---
+++ graphql/server/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// start server
+
+async function startApolloServer() {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs: `
+        type Todo{
+            id: ID!
+            title: String!
+            completed: Boolean!
+        }
+        
+        type Query{
+            getTodo: [Todo]
+        }
+    `,
+    resolvers: {},
+    context: ({ req }) => {
+      return {
+        headers: req.headers,
+      };
+    },
+  });
+  app.use(cors());
+  app.use(bodyParser.json());
+  await server.start();
+  app.use("/graphql", expressMiddleware(server));
+  //   server.applyMiddleware({
+  //     app,
+  //     path: "/graphql",
+  //   });
+
+  app.listen({ port: 4000 }, () => {
+    console.log(`Server ready at http://localhost:4000`);
+  });
+}
+startApolloServer().catch((error) => {
+  console.error(error);
+});

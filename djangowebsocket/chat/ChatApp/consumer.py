e796aa38176a004a2dabf80a302e7a1d8c5a87e7
Leah Deshar <leah.desar@gmail.com>
2024-04-30 19:44:26 +0545

    added send and receive controller

--- djangowebsocket/chat/ChatApp/consumer.py
+++ djangowebsocket/chat/ChatApp/consumer.py
@@ -3,9 +3,8 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from ChatApp.models import *
 
-
 class ChatConsumer(AsyncWebsocketConsumer):
-    
+
     async def connect(self):
         self.room_name = f"room_{self.scope['url_route']['kwargs']['room_name']}"
         await self.channel_layer.group_add(self.room_name, self.channel_name)
@@ -13,3 +12,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_name, self.channel_name)
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json
+        print(message)
+        
+    async def send_message(self, event):
+        data = event['message']
+        await self.create_message(data=data)
+        response_data = {
+            'sender': data['sender'],
+            'message': data['message']
+        }
+        await self.send(text_data=json.dumps({'message': response_data}))

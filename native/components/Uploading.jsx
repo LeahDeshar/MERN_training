6348639a8147ae122792958791e9929ae4d65bd4
Leah Deshar <leah.desar@gmail.com>
2024-05-04 13:04:25 +0545

    upload controller

--- native/components/Uploading.jsx
+++ native/components/Uploading.jsx
@@ -1,6 +1,7 @@
 import { Image, Text, StyleSheet, View, TouchableOpacity } from "react-native";
 import { BlurView } from "expo-blur"; // Import BlurView from expo-blur
 import { Video } from "expo-av";
+import ProgressBar from "./ProgressBar";
 
 export function Uploading({ image, video, progress }) {
   return (
@@ -8,6 +9,7 @@ export function Uploading({ image, video, progress }) {
       style={[
         StyleSheet.absoluteFill,
         {
+          flex: 1,
           alignItems: "center",
           justifyContent: "center",
           zIndex: 1,
@@ -15,8 +17,12 @@ export function Uploading({ image, video, progress }) {
       ]}
     >
       <BlurView // Use BlurView from expo-blur
-        intensity={10}
-        style={StyleSheet.absoluteFill}
+        intensity={20}
+        style={[
+          StyleSheet.absoluteFill,
+          { flex: 1, justifyContent: "center", alignItems: "center" },
+        ]}
+        tint="systemThinMaterialDark"
       >
         <View
           style={{
@@ -25,10 +31,10 @@ export function Uploading({ image, video, progress }) {
             paddingVertical: 16,
             rowGap: 12,
             borderRadius: 14,
-            backgroundColor: "rgba(255, 255, 255, 0.5)", // Adjust background color and opacity as needed
+            backgroundColor: "rgb(255, 255, 255)", // Adjust background color and opacity as needed
           }}
         >
-          {/* {image && (
+          {image && (
             <Image
               source={{ uri: image }}
               style={{
@@ -51,9 +57,9 @@ export function Uploading({ image, video, progress }) {
               resizeMode="contain"
               style={{ width: 200, height: 200 }}
             />
-          )} */}
+          )}
           <Text style={{ fontSize: 12 }}>Uploading...</Text>
-          {/* <ProgressBar progress={progress} /> */}
+          <ProgressBar progress={progress} />
           <View
             style={{
               height: 1,
--- native/screens/Home.jsx
+++ native/screens/Home.jsx
@@ -1,9 +1,33 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import EmptyState from "../components/EmptyState";
 import ProgressBar from "../components/ProgressBar";
 import { Uploading } from "../components/Uploading";
+import { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import { Ionicons } from "@expo/vector-icons";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import {} from "firebase/firestore";
 
 const Home = () => {
+  const [image, setImage] = useState("");
+  const [video, setVideo] = useState("");
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [3, 4],
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  async function uploadImage(uri, fileType) {
+    const response = await fetch(uri);
+    const blob = await response.blob();
+  }
   return (
     <View
       style={{
@@ -12,9 +36,41 @@ const Home = () => {
         alignItems: "center",
       }}
     >
-      <Uploading />
-      {/* <EmptyState /> */}
-      <ProgressBar progress={20} />
+      {image && <Uploading progress={20} />}
+      {/* <Uploading progress={20} /> */}
+      <EmptyState />
+      <TouchableOpacity
+        onPress={pickImage}
+        style={{
+          position: "absolute",
+          bottom: 90,
+          right: 30,
+          width: 44,
+          height: 44,
+          backgroundColor: "black",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: 25,
+        }}
+      >
+        <Ionicons name="image" size={24} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity
+        // onPress={pickVideo}
+        style={{
+          position: "absolute",
+          bottom: 150,
+          right: 30,
+          width: 44,
+          height: 44,
+          backgroundColor: "black",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: 25,
+        }}
+      >
+        <Ionicons name="videocam" size={24} color="white" />
+      </TouchableOpacity>
     </View>
   );
 };

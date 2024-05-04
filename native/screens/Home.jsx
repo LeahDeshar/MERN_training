20844b3de3d7ed8a92dccb994a5f1f3a992ebedb
Leah Deshar <leah.desar@gmail.com>
2024-05-04 12:16:47 +0545

    progress bar

--- native/screens/Home.jsx
+++ native/screens/Home.jsx
@@ -1,6 +1,7 @@
 import { View, Text } from "react-native";
 import EmptyState from "../components/EmptyState";
 import ProgressBar from "../components/ProgressBar";
+import { Uploading } from "../components/Uploading";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
         alignItems: "center",
       }}
     >
+      <Uploading />
       {/* <EmptyState /> */}
       <ProgressBar progress={20} />
     </View>

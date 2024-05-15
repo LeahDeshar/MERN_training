4b34f67915a0af7a291614f2831bd2741933463e
Leah Deshar <leah.desar@gmail.com>
2024-05-15 11:05:36 +0545

    angular 17

--- addtocart/src/app/app.routes.ts
+++ addtocart/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  //   {
+  //     path: 'home',
+  //     component: HomeComponent
+  //   },
+];

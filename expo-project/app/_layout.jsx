23504f33765f417dc7e5a45a40575789c5b00509
Leah Deshar <leah.desar@gmail.com>
2024-06-04 14:50:55 +0545

    theme provider

--- expo-project/app/_layout.jsx
+++ expo-project/app/_layout.jsx
@@ -1,12 +1,25 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Switch, StyleSheet, useColorScheme } from "react-native";
+import React, { useState } from "react";
 
 const Layout = () => {
+  const [toggle, setToggle] = useState(false);
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+  console.log(colorScheme);
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Text>Theme Changer</Text>
+
+      <Switch value={colorScheme == "dark"} onChange={toggleColorScheme} />
     </View>
   );
 };
 
 export default Layout;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});

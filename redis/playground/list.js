5b29eea3c02474dbdda64df08e0d0cc681ad7d12
Leah Deshar <leah.desar@gmail.com>
2024-07-09 18:37:17 +0545

    redis: list

---
+++ redis/playground/list.js
@@ -0,0 +1,12 @@
+const client = require("./client");
+
+async function init() {
+  // Get all keys
+  await client.lpush("messages", 1);
+  await client.lpush("messages", 2);
+  await client.lpush("messages", 3);
+  await client.lpush("messages", 4);
+
+  const result = await client.rpop("messages");
+  console.log(result);
+}

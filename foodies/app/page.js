986b01755fa86be99c882f4774aed73f6dd01b4f
Leah Deshar <leah.desar@gmail.com>
2024-08-25 17:35:04 +0545

    slide show

--- foodies/app/page.js
+++ foodies/app/page.js
@@ -1,20 +1,60 @@
 import Link from "next/link";
-
+import classes from "./page.module.css";
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>
-        Time to get started!
-      </h1>
-      <p>
-        <Link href="/meals">Meals</Link>
-      </p>
-      <p>
-        <Link href="/meals/share">Share a Meal</Link>
-      </p>
-      <p>
-        <Link href="/community">Community</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}></div>
+        <div className={classes.hero}>
+          <h1>NextLevel Food for NextLevel Foodies</h1>
+          <p>Delicious meals, shared by a food-loving community.</p>
+        </div>
+        <div className={classes.cta}>
+          <Link href="/community">Join the community</Link>
+          <Link href="/meals">Expore Meals</Link>
+        </div>
+      </header>
+      <main>
+        <section className={classes.section}>
+          <h2>How it works</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+
+        <section className={classes.section}>
+          <h2>Why NextLevel Food?</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+      </main>
+    </>
+    // <main>
+    //   <h1 style={{ color: "white", textAlign: "center" }}>
+    //     Time to get started!
+    //   </h1>
+    //   <p>
+    //     <Link href="/meals">Meals</Link>
+    //   </p>
+    //   <p>
+    //     <Link href="/meals/share">Share a Meal</Link>
+    //   </p>
+    //   <p>
+    //     <Link href="/community">Community</Link>
+    //   </p>
+    // </main>
   );
 }

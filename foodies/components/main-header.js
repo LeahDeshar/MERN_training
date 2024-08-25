7202f835c541ed82a21ab6e16f9efe71aac3bd55
Leah Deshar <leah.desar@gmail.com>
2024-08-25 17:05:37 +0545

    image and optimization

---
+++ foodies/components/main-header.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import imgLogo from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import Image from "next/image";
+export default function MainHeader() {
+  return (
+    <header className={classes.header}>
+      <Link className={classes.logo} href={"/"}>
+        <Image src={imgLogo} alt="logo" width={200} height={100} />
+        NextLevel Food
+      </Link>
+
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <Link href={"/"}>Browse Meals</Link>
+          </li>
+          <li>
+            <Link href={"/community"}>Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}

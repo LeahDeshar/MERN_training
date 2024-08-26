92344915a89dce967c6bec9e22a64195ecc594fa
Leah Deshar <leah.desar@gmail.com>
2024-08-26 06:48:56 +0545

    next db

--- foodies/components/main-header/nav-link.js
+++ foodies/components/main-header/nav-link.js
@@ -2,13 +2,17 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import classes from "./main-header.module.css";
+import classes from "./nav-link.module.css";
 export default function Navlink({ href, children }) {
   const path = usePathname();
   return (
     <Link
       href={href}
-      className={path.startsWith(href) ? classes.active : undefined}
+      className={
+        path.startsWith(href)
+          ? `${classes.link} ${classes.active}`
+          : classes.link
+      }
     >
       {children}
     </Link>

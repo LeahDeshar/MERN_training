2296b91e7a103282b0f1717f1a1b899685ad0217
Leah Deshar <leah.desar@gmail.com>
2024-08-26 06:05:19 +0545

    rsc and cc

--- foodies/app/page.js
+++ foodies/app/page.js
@@ -1,17 +1,20 @@
 import Link from "next/link";
 import classes from "./page.module.css";
+import ImageSlideshow from "@/components/images/image-slideshow";
 export default function Home() {
   return (
     <>
       <header className={classes.header}>
-        <div className={classes.slideshow}></div>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
+        </div>
         <div className={classes.hero}>
           <h1>NextLevel Food for NextLevel Foodies</h1>
           <p>Delicious meals, shared by a food-loving community.</p>
-        </div>
-        <div className={classes.cta}>
-          <Link href="/community">Join the community</Link>
-          <Link href="/meals">Expore Meals</Link>
+          <div className={classes.cta}>
+            <Link href="/community">Join the community</Link>
+            <Link href="/meals">Expore Meals</Link>
+          </div>
         </div>
       </header>
       <main>

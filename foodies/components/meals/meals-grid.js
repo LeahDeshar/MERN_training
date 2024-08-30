1ffad3fc3a9fe6175c402f554fffe22336af9ce5
Leah Deshar <leah.desar@gmail.com>
2024-08-30 23:34:59 +0545

    meals grid:

--- foodies/components/meals/meals-grid.js
+++ foodies/components/meals/meals-grid.js
@@ -1,15 +1,15 @@
-// import MealItem from "./meal-item";
-// import classes from "./meals-grid.module.css";
+import MealItem from "./meal-item";
+import classes from "./meals-grid.module.css";
 
-// export default function MealGrid({ meals }) {
-//   return (
-//     <ul className={classes.meals}>
-//       {meals.map((meal) => (
-//         <li key={meal.id}>
-//           <MealItem {...meals} />
-//           {meal.name}
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
+export default function MealGrid({ meals }) {
+  return (
+    <ul className={classes.meals}>
+      {meals.map((meal) => (
+        <li key={meal.id}>
+          <MealItem {...meals} />
+          {meal.name}
+        </li>
+      ))}
+    </ul>
+  );
+}

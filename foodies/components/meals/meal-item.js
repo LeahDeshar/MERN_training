c8638408117231654af6e56206819d4ac91adcf6
Leah Deshar <leah.desar@gmail.com>
2024-09-13 10:28:00 +0545

    meal fetched from the db

--- foodies/components/meals/meal-item.js
+++ foodies/components/meals/meal-item.js
@@ -4,11 +4,14 @@ import Image from "next/image";
 import classes from "./meal-item.module.css";
 
 export default function MealItem({ title, slug, image, summary, creator }) {
+  console.log({ title, slug, image, summary, creator });
   return (
     <article className={classes.meal}>
       <header>
         <div className={classes.image}>
-          <Image src={image} alt={title} fill />
+          {/* <Image src={`${"/assets/" + image}`} alt={title} fill /> */}
+
+          <Image src={image} fill alt={title} />
         </div>
         <div className={classes.headerText}>
           <h2>{title}</h2>

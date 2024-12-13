7fa5d4e718b5a954a2a5339fe3a98d7365ee4283
Leah Deshar <leah.desar@gmail.com>
2024-12-13 11:24:18 +0545

    react js task

--- task/task_1/src/two/Filter.tsx
+++ task/task_1/src/two/Filter.tsx
@@ -2,6 +2,10 @@ import React, { useEffect, useMemo, useState } from "react";
 import { Product } from "../types/types";
 function Filter() {
   const [data, setData] = useState<Product[] | null>(null);
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const [selectedPriceRange, setSelectedPriceRange] = useState<string | null>(
+    null
+  );
   useEffect(() => {
     try {
       const fetchData = async () => {
@@ -28,21 +32,53 @@ function Filter() {
       ?.map((item) => item.category)
       .filter((value, index, self) => self.indexOf(value) === index);
   }, [data]);
+
+  const handleCategoryChange = (category: string) => {
+    setSelectedCategory(category);
+  };
+  const handlePriceRangeChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSelectedPriceRange(event.target.value);
+  };
+  const filteredCategoryData = useMemo(() => {
+    if (!selectedCategory || !data) return data;
+    return data.filter((item) => item.category === selectedCategory);
+  }, [selectedCategory, data]);
+
   if (!data) return <div>Loading...</div>;
 
   return (
     <div>
-      <h2>Filter</h2>
+      <h2>Filter useMemo simulation</h2>
       <div>
         <h3>Categories</h3>
         <ul className="horizontal-ul">
           {categories?.map((category) => (
-            <li key={category}>{category}</li>
+            <li
+              className={category === selectedCategory ? "active-category" : ""}
+              key={category}
+              onClick={() => handleCategoryChange(category)}
+            >
+              {category}
+            </li>
           ))}
         </ul>
+
+        <div>
+          <label htmlFor="priceRange">Select Price Range: </label>
+          <select id="priceRange" onChange={handlePriceRangeChange}>
+            <option value="">All</option>
+            <option value="0-50">0 - 50</option>
+            <option value="51-100">51 - 100</option>
+            <option value="101-200">101 - 200</option>
+            <option value="201-500">201 - 500</option>
+            <option value="501-1000">501 - 1000</option>
+          </select>
+        </div>
       </div>
       <ul>
-        {filteredData?.map((item) => (
+        {filteredCategoryData?.map((item) => (
           <li key={item.product_id}>
             <div className="container">
               <p>{item.product_name}</p>

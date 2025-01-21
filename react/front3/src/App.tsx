feb32fbbc1cc2f5ef7dd804e1e209df770da3ba3
Leah Deshar <leah.desar@gmail.com>
2025-01-21 20:48:30 +0545

    update: react animation

--- react/front3/src/App.tsx
+++ react/front3/src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Hello</h1>
+        <svg width="100" height="100">
+          <circle
+            cx="50"
+            cy="50"
+            r="40"
+            stroke="black"
+            stroke-width="3"
+            fill="red"
+          >
+            <animate
+              attributeName="r"
+              from="10"
+              to="40"
+              dur="1s"
+              repeatCount="indefinite"
+            />
+          </circle>
+        </svg>
+
+        <svg width="100" height="100">
+          <circle cx="50" cy="50" r="40" fill="red">
+            <animate
+              attributeName="fill"
+              from="red"
+              to="blue"
+              dur="2s"
+              repeatCount="indefinite"
+            />
+          </circle>
+        </svg>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

be1f6b2bc303959e6a78812d41f628ff17f7543d
Leah Deshar <leah.desar@gmail.com>
2025-03-13 11:51:12 +0545

    chore: transaction category updating

--- angular/coinsavvy/src/app/modules/transactions/transactionpage/transactionpage.component.ts
+++ angular/coinsavvy/src/app/modules/transactions/transactionpage/transactionpage.component.ts
@@ -1,12 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-transactionpage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './transactionpage.component.html',
-  styleUrl: './transactionpage.component.css'
+  styleUrl: './transactionpage.component.css',
 })
 export class TransactionpageComponent {
+  isModalOpen = false;
+  searchQuery = '';
+  categories = [
+    'Financial Income',
+    'Income',
+    'Other (income)',
+    'Home',
+    'Family',
+    'Health/Sports',
+    'Pets',
+  ];
+  filteredCategories = [...this.categories];
+  selectedCategory = '';
 
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  filterCategories() {
+    this.filteredCategories = this.categories.filter((category) =>
+      category.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+  }
+
+  selectCategory(category: string) {
+    this.selectedCategory = category;
+    this.closeModal();
+  }
 }

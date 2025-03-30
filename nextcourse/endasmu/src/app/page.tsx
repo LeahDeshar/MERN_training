63cd21d6cda6ec8e78ba399965358b8995c12f77
Leah Deshar <leah.desar@gmail.com>
2025-03-30 16:02:09 +0545

    chore: reponsive completed

--- nextcourse/endasmu/src/app/page.tsx
+++ nextcourse/endasmu/src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Button from "@/components/ui/Button";
+
 import { MoveRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,69 +8,21 @@ import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   return (
-    <div className=" text-white">
+    <div className="relative  flex flex-col justify-center text-white overflow-hidden px-4 sm:px-6 lg:px-18 py-14   ">
       <Header />
       <MainBody />
+      <div className="absolute left-0 top-0 w-[800px] h-[500px] -z-40">
+        <Image
+          src="/hero/hero.svg"
+          layout="fill"
+          objectFit="cover"
+          alt="Hero Image"
+        />
+      </div>
       <GlassContent />
+      <ToplistArtist />
 
-      <section className="px-4 lg:px-16  overflow-hidden relative min-w-[32rem] ">
-        <div className="px-4 lg:px-16 gap-20 py-14  flex flex-col sm:flex-row items-center  ">
-          <div>
-            <h1 className="text-lg font-semibold">Popular Item</h1>
-            <div className="text-7xl sm:text-7xl lg:text-5xl font-bold mt-8 space-y-2">
-              <p>Hot Trending</p>
-              <p>On This</p>
-              <p>Week.</p>
-            </div>
-
-            <p className="mt-4 text-gray-300 max-w-lg text-justify">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laboriosam consequuntur reiciendis, nisi, commodi sit fugit
-              debitis illum ut, quasi sunt. Laudantium optio laborum porro
-              blanditiis, odio soluta maxime inventore !
-            </p>
-
-            <button className="flex items-center mt-6 text-lg font-medium text-white hover:underline">
-              See all
-              <MoveRight className="ml-2 w-6 h-6" />
-            </button>
-          </div>
-
-          <div className="flex flex-col sm:flex-row gap-6 lg:self-end lg:ml-auto">
-            <ProfileCard />
-            <ProfileCard />
-          </div>
-          <div className="absolute  -right-56 w-[500px] h-[500px] -z-40">
-            <Image
-              src="/hero/hero_2.svg"
-              layout="fill"
-              objectFit="cover"
-              alt="Hero Image"
-            />
-          </div>
-        </div>
-      </section>
-
-      <section className="relative px-4 lg:px-28 gap-20 py-24 min-w-[32rem] border border-white">
-        <div className="text-white text-8xl  pt-14 pb-8">
-          <h1 className="my-5">Subscribe to Get Fresh </h1>
-          <h1 className="my-5">News Update About </h1>
-          <h1 className="my-5">NFTs.</h1>
-        </div>
-
-        <button className="rounded-full px-6 py-2 mt-5 bg-gradient-to-br from-[#36b3c9] via-[#2a8c9d] via-30% to-[#32ad5e] text-black text-lg">
-          Subscribe
-        </button>
-
-        <div className="absolute top-0  -left-72 w-[800px] h-[800px] -z-40">
-          <Image
-            src="/hero/hero_3.svg"
-            layout="fill"
-            objectFit="cover"
-            alt="Hero Image"
-          />
-        </div>
-      </section>
+      <Subscribe />
       <Footer />
     </div>
   );
@@ -78,8 +30,8 @@ export default function Home() {
 
 const ProfileCard = () => {
   return (
-    <div className="relative bg-[#000] text-white rounded-4xl h-96  lg:h-80 w-full sm:w-72 lg:w-64 flex flex-col items-center justify-between shadow-[inset_4px_4px_8px_rgba(255,255,255,0.25),_inset_-4px_-4px_8px_rgba(0,0,0,0.6)] transition-all duration-300 lg:mt-0 mt-16">
-      <div className="absolute -top-6 w-20 h-20 lg:w-16 lg:h-16 sm:w-20 sm:h-20 rounded-full shadow-md shadow-white/50 overflow-hidden">
+    <div className="relative bg-[#000] text-white rounded-4xl h-96 [@media_(min-width:679px)_and_(max-width:930px)]:h-56 lg:h-80 w-full sm:w-72 lg:w-64 flex flex-col items-center justify-between shadow-[inset_4px_4px_8px_rgba(255,255,255,0.25),_inset_-4px_-4px_8px_rgba(0,0,0,0.6)] transition-all duration-300 lg:mt-0 [@media_(min-width:679px)_and_(max-width:930px)]:mt-10 mt-16 ">
+      <div className="absolute -top-6 w-20 h-20  lg:w-16 lg:h-16 sm:w-20 sm:h-20 rounded-full shadow-md shadow-white/50 overflow-hidden [@media_(min-width:679px)_and_(max-width:930px)]:w-16 [@media_(min-width:679px)_and_(max-width:930px)]:h-16">
         <Image
           src="/profiles/p1.jpg"
           layout="fill"
@@ -97,14 +49,16 @@ const ProfileCard = () => {
           alt="Crystal Icon"
           objectFit="cover"
         />
-        <h2 className="text-lg sm:text-xl font-semibold mt-2">Johnny Five</h2>
-        <p className="text-sm sm:text-base text-gray-300 mt-1 px-2 sm:px-4">
+        <h2 className="text-lg sm:text-xl [@media_(min-width:679px)_and_(max-width:930px)]:text-md font-semibold mt-2">
+          Johnny Five
+        </h2>
+        <p className="text-lg lg:text-sm [@media_(min-width:679px)_and_(max-width:930px)]:text-sm sm:text-base text-gray-300 mt-1 px-2 sm:px-4">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam,
           ut.
         </p>
       </div>
 
-      <div className="w-[90%] h-32 lg:h-28 sm:h-32 rounded-4xl relative overflow-hidden mt-4">
+      <div className="w-[90%] h-32 lg:h-28 sm:h-32 rounded-4xl relative overflow-hidden [@media_(min-width:679px)_and_(max-width:930px)]:mt-0  [@media_(min-width:679px)_and_(max-width:930px)]:h-16 mt-4">
         <Image
           src="/profiles/p5.jpg"
           layout="fill"
@@ -120,10 +74,8 @@ const ProfileCard = () => {
 const MainBody = () => {
   return (
     <main
-      className="relative lg:bg-cover lg:bg-center  sm:bg-left bg-no-repeat min-h-screen flex flex-col justify-center overflow-hidden px-4 sm:px-6 lg:px-18 py-14 sm:py-24 w-full min-w-[32rem]"
-      style={{
-        backgroundImage: "url('/hero/hero.svg')",
-      }}
+    // [@media_(min-width:679px)_and_(max-width:1087px)]:py-0
+    // className="relative lg:bg-cover lg:bg-center  sm:bg-left bg-no-repeat flex flex-col justify-center overflow-hidden px-4 sm:px-6 lg:px-18 py-14   "
     >
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 sm:px-6 py-10 sm:py-20 ">
         <div className="text-white text-left mt-12 lg:mt-0 sm:mt-9">
@@ -156,7 +108,6 @@ const MainBody = () => {
           </div>
         </div>
 
-        {/* Right Section */}
         <div className="flex justify-center">
           <Image
             src="/hero/hero_5.png"
@@ -298,7 +249,7 @@ const Header = () => {
 };
 const GlassContent = () => {
   return (
-    <div className="max-w-[88rem] min-w-[32rem] w-full px-4 lg:px-16 mx-auto relative">
+    <div className="max-w-[88rem] min-w-[25rem] w-full mx-auto relative">
       <div className="isolate bg-white/5 shadow-sm shadow-amber-50 ring-1 ring-black/5 mx-auto w-[95%] rounded-4xl p-6 relative">
         <div className="px-4 sm:px-6 flex flex-col md:flex-row justify-between items-center py-10 gap-8 text-center md:text-left">
           <div>
@@ -361,7 +312,7 @@ const GlassContent = () => {
         </div>
       </div>
 
-      <section className="flex flex-col-reverse md:flex-row items-center sm:px-4 px-4 lg:px-0 gap-8 md:gap-20  py-24  w-full ">
+      <section className="flex flex-col-reverse md:flex-row items-center sm:px-0 px-0 lg:px-0 gap-8 md:gap-20  py-24  w-full ">
         <div className="container max-w-screen-xl mx-auto flex flex-col-reverse md:flex-row items-center gap-8 md:gap-20">
           <div className="flex justify-center md:justify-start w-full">
             <Image
@@ -399,14 +350,78 @@ const GlassContent = () => {
     </div>
   );
 };
+const ToplistArtist = () => {
+  return (
+    <section className="px-0 lg:px-16   w-full min-w-full ">
+      <div className="px-0 lg:px-16 gap-20 py-14  flex flex-col sm:flex-row items-center  ">
+        <div>
+          <h1 className="text-lg font-semibold">Artist</h1>
+          <div className="text-7xl sm:text-7xl md:text-[3.2rem] lg:text-5xl font-bold mt-8 space-y-2">
+            <p>Top List</p>
+            <p>Artist.</p>
+          </div>
 
+          <p className="mt-4 text-gray-300 max-w-lg text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+            consequuntur reiciendis, nisi, commodi sit fugit debitis illum ut,
+            quasi sunt. Laudantium optio laborum porro blanditiis, odio soluta
+            maxime inventore !
+          </p>
+
+          <button className="flex items-center mt-6 text-lg font-medium text-white hover:underline">
+            See all
+            <MoveRight className="ml-2 w-6 h-6" />
+          </button>
+        </div>
+
+        <div className="flex flex-col sm:flex-row gap-6 lg:self-end lg:ml-auto  [@media_(min-width:679px)_and_(max-width:930px)]:flex-col">
+          <ProfileCard />
+          <ProfileCard />
+        </div>
+        <div className="absolute  -right-56 w-[500px] h-[500px] -z-40">
+          <Image
+            src="/hero/hero_2.svg"
+            layout="fill"
+            objectFit="cover"
+            alt="Hero Image"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Subscribe = () => {
+  return (
+    <section className="relative px-4 lg:px-28 gap-20 py-24 min-w-[25rem] ">
+      <div className="text-white text-5xl lg:text-7xl  pt-14 pb-8">
+        <h1 className="my-5">Subscribe to Get Fresh </h1>
+        <h1 className="my-5">News Update About </h1>
+        <h1 className="my-5">NFTs.</h1>
+      </div>
+
+      <button className="rounded-full lg:w-auto w-[23rem] px-6 py-2 mt-5 bg-gradient-to-br from-[#36b3c9] via-[#2a8c9d] via-30% to-[#32ad5e] text-black text-lg">
+        Subscribe
+      </button>
+
+      <div className="absolute top-0 -left-72 w-[800px] h-[800px] -z-40">
+        <Image
+          src="/hero/hero_3.svg"
+          layout="fill"
+          objectFit="cover"
+          alt="Hero Image"
+        />
+      </div>
+    </section>
+  );
+};
 const Footer = () => {
   return (
-    <footer className="relative overflow-hidden text-white py-12 px-6 lg:px-28">
+    <footer className=" w-full overflow-hidden text-white py-12 px-6 lg:px-28 min-w-[26rem]">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-2 ">
         <div className="lg:col-span-2">
-          <h1 className="text-2xl font-bold mb-4">Endasmu</h1>
-          <p className="text-gray-400 text-sm">
+          <h1 className="text-3xl lg:text-2xl font-bold mb-4">Endasmu</h1>
+          <p className="text-gray-400 text-lg lg:text-sm">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa harum
             corporis, rem beatae voluptas vitae a odio perspiciatis odit
             voluptatibus!
@@ -430,13 +445,15 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="justify-self-end">
-          <h1 className="text-xl font-semibold mb-4">Explore</h1>
+        <div className="justify-self-start lg:justify-self-end">
+          <h1 className="text-2xl lg:text-xl lg:mt-0 mt-8 font-semibold mb-4">
+            Explore
+          </h1>
           {["Art", "Collections", "Domain Name", "Utility"].map(
             (item, index) => (
               <p
                 key={index}
-                className="text-gray-400 text-sm mb-2 hover:text-white"
+                className="text-gray-400 text-lg lg:text-sm mb-2 hover:text-white"
               >
                 {item}
               </p>
@@ -444,25 +461,29 @@ const Footer = () => {
           )}
         </div>
 
-        <div className="justify-self-end">
-          <h1 className="text-xl font-semibold mb-4">Statistic</h1>
+        <div className="justify-self-start lg:justify-self-end">
+          <h1 className="text-2xl lg:text-xl lg:mt-0 mt-8 font-semibold mb-4">
+            Statistic
+          </h1>
           {["Ranking", "Activity"].map((item, index) => (
             <p
               key={index}
-              className="text-gray-400 text-sm mb-2 hover:text-white"
+              className="text-gray-400 text-lg lg:text-sm mb-2 hover:text-white"
             >
               {item}
             </p>
           ))}
         </div>
 
-        <div className="justify-self-end">
-          <h1 className="text-xl font-semibold mb-4">Resource</h1>
+        <div className="justify-self-start lg:justify-self-end">
+          <h1 className="text-2xl lg:text-xl lg:mt-0 mt-8 font-semibold mb-4">
+            Resource
+          </h1>
           {["Help Center", "Platform Status", "Partners", "Blog", "Faq"].map(
             (item, index) => (
               <p
                 key={index}
-                className="text-gray-400 text-sm mb-2 hover:text-white"
+                className="text-gray-400 text-lg lg:text-sm mb-2 hover:text-white"
               >
                 {item}
               </p>
@@ -470,12 +491,14 @@ const Footer = () => {
           )}
         </div>
 
-        <div className="justify-self-end">
-          <h1 className="text-xl font-semibold mb-4">Company</h1>
+        <div className="justify-self-start lg:justify-self-end">
+          <h1 className="text-2xl lg:text-xl lg:mt-0 mt-8 font-semibold mb-4">
+            Company
+          </h1>
           {["About us", "Career", "Support"].map((item, index) => (
             <p
               key={index}
-              className="text-gray-400 text-sm mb-2 hover:text-white"
+              className="text-gray-400 text-lg lg:text-sm mb-2 hover:text-white"
             >
               {item}
             </p>
@@ -483,7 +506,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="absolute top-0 opacity-70 -right-56 w-[500px] h-[500px] -z-40">
+      <div className="absolute lg:bottom-10 bottom-150 md:bottom-30 sm:bottom-150 opacity-70 -right-56 w-[500px] h-[500px] -z-40">
         <Image
           src="/hero/hero_2.svg"
           layout="fill"
@@ -494,45 +517,3 @@ const Footer = () => {
     </footer>
   );
 };
-// BACKUP
-{
-  /* <div className="grid grid-cols-2   px-10 ">
-          <div className="text-white">
-            <div>
-              <p className="mt-4 text-7xl">Discover</p>
-              <p className="mt-4 text-7xl">Digital Art and </p>
-              <p className="mt-4 text-7xl">Collect NFTS.</p>
-            </div>
-            <div className=" py-5">
-              <p className="py-9">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
-                blanditiis mollitia eos magni? Cumque quisquam unde assumenda
-                possimus officia magni sunt ullam voluptates porro nemo
-                nesciunt, laboriosam excepturi repudiandae ducimus?
-              </p>
-              <div className="space-x-12  flex items-center">
-                <button
-                  className="rounded-full px-6 py-2 bg-gradient-to-br from-[#36b3c9] via-[#2a8c9d] via-30% to-[#32ad5e] text-black"
-
-                  // className="rounded-full px-6 py-2 bg-gradient-to-r from-[#dfd056] via-[#bdc96f] via-30% to-[#81bd51] text-black"
-                >
-                  Get Started
-                </button>
-                <button className="flex items-center">
-                  Learn More
-                  <MoveRight width={50} />
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="flex justify-center ">
-          
-            <Image
-              src="/hero/hero_5.png"
-              width={500}
-              height={500}
-              alt="Hero Image"
-            />
-          </div>
-        </div> */
-}
